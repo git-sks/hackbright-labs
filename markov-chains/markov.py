386dae2aa821ef5808f301a2a04c1769aa769d4c
Stephanie Shih <stephk.shih@gmail.com>
2020-05-06 13:02:33 -0700

    Added .gitignore file. Completed make_text function in markov.py.

--- markov-chains/markov.py
+++ markov-chains/markov.py
@@ -48,17 +48,16 @@ def make_chains(text_string):
     words = text_string.split()
 
     # Go through the words and form chains consisting of the word at the index
-    # and the word that follows. Then add it to the chains dictionary
-    for index in range(len(words) - 1):
+    # and the word that follows, excluding the last bigram.
+    # Then add it to the chains dictionary
+    for index in range(len(words) - 2):
         # Form a chain from the word at index, and the word that follows
         chain = (words[index], words[index + 1])
 
-        # If not the last two words in the text, make the tuple a key in chains,
-        # add the word that follows those two words to the list value
-        # If last two words, nothing follows, so nothing to append.
-        if not index == len(words) - 2:
-            chains[chain] = chains.get(chain, [])
-            chains[chain].append(words[index + 2])
+        # Make the tuple a key in chains, and add the word that follows those
+        # two words to the list value.
+        chains[chain] = chains.get(chain, [])
+        chains[chain].append(words[index + 2])
 
     return chains
 
@@ -69,11 +68,38 @@ def make_text(chains):
     words = []
 
     # your code goes here
+    # Get a random key to start off with as the initial link
+    link = choice(list(chains.keys()))
 
-    return " ".join(words)
+    # Add the initial link to the list of words
+    words.extend(link)
+
+    # Get a random follow up word, and add it to words list
+    follow_up_word = choice(chains[link])
+
+    try:
+        # Loop through the append-link maker-follow up word lookup
+        # until you hit a KeyError (have reached a link that doesn't exist in
+        # chains dictionary)
+        while True:
+            # Add the follow up word to the words list
+            words.append(follow_up_word)
+
+            # Create a new link from the 2nd word of the current link and the
+            # follow up word, and overwrite so the current link is the new link
+            link = (link[1], follow_up_word)
+
+            # Look up this new link in the dictionary of chains and make
+            # the follow up word a random word from the new link's values
+            follow_up_word = choice(chains[link])
+    except KeyError:
+        # Have hit end of possible follow ups, return a string of the words in
+        # words list joined together
+        return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+# input_path = "green-eggs.txt"
+input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
