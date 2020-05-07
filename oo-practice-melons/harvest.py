234ec14283d74f361d60a336836a9dbfe6260173
Stephanie Shih <stephk.shih@gmail.com>
2020-05-07 12:41:24 -0700

    Defined MelonType class, completed make_melon_types function.

--- oo-practice-melons/harvest.py
+++ oo-practice-melons/harvest.py
@@ -13,16 +13,24 @@ class MelonType(object):
         self.pairings = []
 
         # Fill in the rest
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
         # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
@@ -31,8 +39,23 @@ def make_melon_types():
     all_melon_types = []
 
     # Fill in the rest
+    musk = MelonType('musk', 1988, 'green', True, True, 'Muskmelon')
+    musk.add_pairing('mint')
+
+    cas = MelonType('cas', 2003, 'orange', False, False, 'Casaba')
+    cas.add_pairing('strawberries')
+    cas.add_pairing('mint')
+
+    cren = MelonType('cren', 1996, 'green', False, False, 'Crenshaw')
+    cren.add_pairing('prosciutto')
+
+    yw = MelonType('yw', 2013, 'yellow', False, True, 'Yellow Watermelon')
+    yw.add_pairing('ice cream')
+
+    all_melon_types.extend([musk, cas, cren, yw])
 
     return all_melon_types
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
